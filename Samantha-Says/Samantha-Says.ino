a498dd1d441257ccea94d1d53f32223eb1ab5a50
Lukas2065 <lukasjonkus03@gmail.com>
2026-01-30 15:32:10 +0000

    Update Samantha-Says.ino
    Created a startup sequence. Created an FSM that does a game start sequence followed by picking a random LED and then allowing the player to press an LED.

--- Samantha-Says/Samantha-Says.ino
+++ Samantha-Says/Samantha-Says.ino
@@ -8,8 +8,11 @@
 #define BLUE_SWITCH 8
 #define YELLOW_SWITCH 9
 
+enum {start_game, random_LED, wait_for_response} current_state;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
@@ -19,19 +22,63 @@ void setup() {
   pinMode(GREEN_SWITCH, INPUT_PULLUP);
   pinMode(BLUE_SWITCH, INPUT_PULLUP);
   pinMode(YELLOW_SWITCH, INPUT_PULLUP);
+
+  current_state = start_game;
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  light_led_with_switch();
+  switch (current_state) {
+    case start_game:
+      start_game_sequence();
+      current_state = random_LED;
+      break;
+    case random_LED:
+      light_up_random_led();
+      current_state = wait_for_response;
+      break;
+    case wait_for_response:
+      if(light_led_with_switch() != -1) {
+        current_state = random_LED;
+      }
+      break;
+  }
 
 }
 
-void light_led_with_switch() {
+void light_up_random_led() {
+  int random_num = random(0,4);
+  Serial.println(random_num);
+
+  switch (random_num) {
+    case 0:
+      digitalWrite(RED_LED, HIGH);
+      delay(200);
+      all_led_states(false);
+      break;
+    case 1:
+      digitalWrite(GREEN_LED, HIGH);
+      delay(200);
+      all_led_states(false);
+      break;
+    case 2:
+      digitalWrite(BLUE_LED, HIGH);
+      delay(200);
+      all_led_states(false);
+      break;
+    case 3:
+      digitalWrite(YELLOW_LED, HIGH);
+      delay(200);
+      all_led_states(false);
+      break;
+  }
+}
+
+int light_led_with_switch() {
   if (digitalRead(RED_SWITCH) == LOW)
   {
     digitalWrite(RED_LED, HIGH);
+    return 0;
   }
   if (digitalRead(RED_SWITCH) == HIGH)
   {
@@ -41,6 +88,7 @@ void light_led_with_switch() {
   if (digitalRead(GREEN_SWITCH) == LOW)
   {
     digitalWrite(GREEN_LED, HIGH);
+    return 1;
   }
   if (digitalRead(GREEN_SWITCH) == HIGH)
   {
@@ -50,6 +98,7 @@ void light_led_with_switch() {
   if (digitalRead(BLUE_SWITCH) == LOW)
   {
     digitalWrite(BLUE_LED, HIGH);
+    return 2;
   }
   if (digitalRead(BLUE_SWITCH) == HIGH)
   {
@@ -59,9 +108,74 @@ void light_led_with_switch() {
   if (digitalRead(YELLOW_SWITCH) == LOW)
   {
     digitalWrite(YELLOW_LED, HIGH);
+    return 3;
   }
   if (digitalRead(YELLOW_SWITCH) == HIGH)
   {
+    digitalWrite(YELLOW_LED, LOW);
+  }
+
+  return -1;
+}
+
+void start_game_sequence() {
+  digitalWrite(RED_LED, HIGH);
+  delay(100);
+  digitalWrite(GREEN_LED, HIGH);
+  delay(100);
+  digitalWrite(BLUE_LED, HIGH);
+  delay(100);
+  digitalWrite(YELLOW_LED, HIGH);
+  delay(100);
+  digitalWrite(RED_LED, LOW);
+  delay(100);
+  digitalWrite(GREEN_LED, LOW);
+  delay(100);
+  digitalWrite(BLUE_LED, LOW);
+  delay(100);
+  digitalWrite(YELLOW_LED, LOW);
+  delay(100);
+
+  digitalWrite(YELLOW_LED, HIGH);
+  delay(100);
+  digitalWrite(BLUE_LED, HIGH);
+  delay(100);
+  digitalWrite(GREEN_LED, HIGH);
+  delay(100);
+  digitalWrite(RED_LED, HIGH);
+  delay(100);
+  digitalWrite(YELLOW_LED, LOW);
+  delay(100);
+  digitalWrite(BLUE_LED, LOW);
+  delay(100);
+  digitalWrite(GREEN_LED, LOW);
+  delay(100);
+  digitalWrite(RED_LED, LOW);
+  delay(200);
+
+  all_led_states(true);
+  delay(200);
+
+  all_led_states(false);
+  delay(200);
+
+  all_led_states(true);
+  delay(200);
+
+  all_led_states(false);
+  delay(100);
+}
+
+void all_led_states(bool isOn) {
+  if(isOn) {
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(BLUE_LED, HIGH);
+    digitalWrite(YELLOW_LED, HIGH);
+  } else {
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(BLUE_LED, LOW);
     digitalWrite(YELLOW_LED, LOW);
   }
 }
