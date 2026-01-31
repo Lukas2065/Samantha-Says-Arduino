c359ca64939d1bc18c974ad51ed18073db7720a5
Lukas2065 <lukasjonkus03@gmail.com>
2026-01-31 11:43:59 +0000

    Update Samantha-Says.ino
    Fixed functionality where user can no longer hold a switch and keep LED light on. If user holds the switch it turns LED on for 1 second and then carries out with rest of operations, if the user is still holding switch then it is counted as a seperate press.

--- Samantha-Says/Samantha-Says.ino
+++ Samantha-Says/Samantha-Says.ino
@@ -8,19 +8,20 @@
 #define BLUE_SWITCH 8
 #define YELLOW_SWITCH 9
 
-enum {start_game, random_LED, play_sequence, wait_for_response} current_state;
+enum {start_game, random_LED, play_sequence, wait_for_response, game_fail} current_state;
 int samantha_sequence[50];
 int user_sequence[50];
 int index;
+int user_choice;
 
 void setup() {
   Serial.begin(9600);
-  
+
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
   pinMode(YELLOW_LED, OUTPUT);
-  
+
   pinMode(RED_SWITCH, INPUT_PULLUP);
   pinMode(GREEN_SWITCH, INPUT_PULLUP);
   pinMode(BLUE_SWITCH, INPUT_PULLUP);
@@ -29,7 +30,7 @@ void setup() {
   randomSeed(analogRead(0));
   current_state = start_game;
   index = 0;
-  
+
 }
 
 void loop() {
@@ -39,7 +40,7 @@ void loop() {
       current_state = random_LED;
       break;
     case random_LED:
-      add_to_samantha_sequence(samantha_sequence, index); 
+      add_to_samantha_sequence(samantha_sequence, index);
       index++;
       current_state = play_sequence;
       break;
@@ -48,27 +49,51 @@ void loop() {
       current_state = wait_for_response;
       break;
     case wait_for_response:
-      if(light_led_with_switch() != -1) {
+      user_choice = light_led_with_switch();
+      if (user_choice != -1) {
+        delay(400);
         current_state = random_LED;
       }
+      break;
+    case game_fail:
+      play_fail();
+      index = 0;
+      current_state = start_game;
       break;
   }
 
 }
 
+void play_fail() {
+  digitalWrite(RED_LED, HIGH);
+  delay(100);
+  digitalWrite(RED_LED, LOW);
+  delay(100);
+  digitalWrite(RED_LED, HIGH);
+  delay(100);
+  digitalWrite(RED_LED, LOW);
+  delay(100);
+  digitalWrite(RED_LED, HIGH);
+  delay(100);
+  digitalWrite(RED_LED, LOW);
+  delay(100);
+}
+
+void check_player_response(int player_choice, int index) {
+  
+}
+
 void play_led_sequence(int samantha_sequence[], int current_index) {
   int current_led;
-  for(int i=0; i<current_index; i++) {
+  for (int i = 0; i < current_index; i++) {
     current_led = samantha_sequence[i];
     light_up_random_led(current_led);
   }
 }
 
 void add_to_samantha_sequence(int samantha_sequence[], int current_index) {
-  int random_num = random(0,4);
+  int random_num = random(0, 4);
   samantha_sequence[current_index] = random_num;
-
-  return random_num;
 }
 
 void light_up_random_led(int led_num) {
@@ -101,44 +126,66 @@ void light_up_random_led(int led_num) {
 }
 
 int light_led_with_switch() {
+  int current_switch = get_switch_pressed();
+  led_state(current_switch, true);
+  delay(200);
+  all_led_states(false);
+  return current_switch;
+}
+
+
+bool is_switch_released(int button) {
+  switch (button) {
+    case 0:
+      if (digitalRead(RED_SWITCH) == HIGH)
+      {
+        return true;
+      }
+      break;
+    case 1:
+      if (digitalRead(GREEN_SWITCH) == HIGH)
+      {
+        return true;
+      }
+      break;
+    case 2:
+      if (digitalRead(BLUE_SWITCH) == HIGH)
+      {
+        return true;
+      }
+      break;
+    case 3:
+      if (digitalRead(YELLOW_SWITCH) == HIGH)
+      {
+        return true;
+      }
+      break;
+    default:
+      return false;
+      break;
+  }
+  return false;
+}
+
+int get_switch_pressed() {
   if (digitalRead(RED_SWITCH) == LOW)
   {
-    digitalWrite(RED_LED, HIGH);
     return 0;
   }
-  if (digitalRead(RED_SWITCH) == HIGH)
-  {
-    digitalWrite(RED_LED, LOW);
-  }
-  
+
   if (digitalRead(GREEN_SWITCH) == LOW)
   {
-    digitalWrite(GREEN_LED, HIGH);
     return 1;
   }
-  if (digitalRead(GREEN_SWITCH) == HIGH)
-  {
-    digitalWrite(GREEN_LED, LOW);
-  }
-  
+
   if (digitalRead(BLUE_SWITCH) == LOW)
   {
-    digitalWrite(BLUE_LED, HIGH);
     return 2;
   }
-  if (digitalRead(BLUE_SWITCH) == HIGH)
-  {
-    digitalWrite(BLUE_LED, LOW);
-  }
-  
+
   if (digitalRead(YELLOW_SWITCH) == LOW)
   {
-    digitalWrite(YELLOW_LED, HIGH);
     return 3;
-  }
-  if (digitalRead(YELLOW_SWITCH) == HIGH)
-  {
-    digitalWrite(YELLOW_LED, LOW);
   }
 
   return -1;
@@ -192,8 +239,42 @@ void start_game_sequence() {
   delay(100);
 }
 
+void led_state(int led, bool isOn) {
+  if (isOn) {
+    switch (led) {
+      case 0:
+        digitalWrite(RED_LED, HIGH);
+        break;
+      case 1:
+        digitalWrite(GREEN_LED, HIGH);
+        break;
+      case 2:
+        digitalWrite(BLUE_LED, HIGH);
+        break;
+      case 3:
+        digitalWrite(YELLOW_LED, HIGH);
+        break;
+    }
+  } else {
+    switch (led) {
+      case 0:
+        digitalWrite(RED_LED, LOW);
+        break;
+      case 1:
+        digitalWrite(GREEN_LED, LOW);
+        break;
+      case 2:
+        digitalWrite(BLUE_LED, LOW);
+        break;
+      case 3:
+        digitalWrite(YELLOW_LED, LOW);
+        break;
+    }
+  }
+}
+
 void all_led_states(bool isOn) {
-  if(isOn) {
+  if (isOn) {
     digitalWrite(RED_LED, HIGH);
     digitalWrite(GREEN_LED, HIGH);
     digitalWrite(BLUE_LED, HIGH);
